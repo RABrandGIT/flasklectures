785cd807180dae17e4a5a1ccbd187a2d37fef2bc
RABrandGIT <rodion.brand@academytrainee.com>
2019-11-28 16:49:52 +0000

    exit() :

--- application/__init__.py
+++ application/__init__.py
@@ -1,4 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+import pymysql
+
+host = os.getenv('MYSQL_HOST')
+user = os.getenv('MYSQL_USER')
+password = os.getenv('MYSQL_PASSWORD')
+db = os.getenv('MYSQL_DATABASE')
+charset = 'utf8mb4'
+
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://os.getenv('MYSQL_USER'):os.getenv('MYSQL_PASSWORD')@35.242.150.181/Testbase'
+db = SQLAlchemy(app)
+
 from application import routes
 
--- application/routes.py
+++ application/routes.py
@@ -1,10 +1,12 @@
 from flask import render_template
 from application import app
+from application.models import Posts
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html', title = 'Home', posts=dummyData)
+    postData = Posts.query.all()
+    return render_template('home.html', title = 'Home', posts=postData)
 @app.route('/about')
 def about():
     return render_template('about.html', title = 'About')
