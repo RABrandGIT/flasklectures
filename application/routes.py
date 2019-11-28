ea409787bd91a8127a48bb9a6284de7091088bc2
RABrandGIT <rodion.brand@academytrainee.com>
2019-11-28 14:08:43 +0000

    dummy variables and shit

--- application/routes.py
+++ application/routes.py
@@ -4,7 +4,7 @@ from application import app
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html', title = 'Home')
+    return render_template('home.html', title = 'Home', posts=dummyData)
 @app.route('/about')
 def about():
     return render_template('about.html', title = 'About')
@@ -14,3 +14,16 @@ def login():
 @app.route('/register')
 def register():
     return render_template('register.html', title = 'Register')
+
+dummyData = [
+    {
+        "name": {"first":"Chester", "last":"Gardner"},
+        "title":"First Post",
+        "content":"This is some dummy data for Flask lectures"
+    },
+    {
+        "name": {"first":"Chris", "last":"Perrins"},
+        "title":"Second Post",
+        "content":"This is even more dummy data for Flask lectures"
+    }
+]
